53209cc888449999ea2f13442453be81c2d5f130
Daniel Kenny <daniel.kenny@impartner.com>
2022-12-01 14:10:29 +0000

    Add project files.

---
+++ Day1/Part1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day1
+{
+	class Part1
+	{
+		public static void Run()
+		{
+			var input = File.ReadAllLines("../../../Day1/Input.txt");
+
+			var totalCalories = 0;
+			var highestCalories = 0;
+
+			foreach(var line in input)
+			{
+				if (line != "")
+				{
+					totalCalories += Int32.Parse(line);
+				}
+				else
+				{
+					if (totalCalories > highestCalories)
+					{
+						highestCalories = totalCalories;
+					}
+					totalCalories = 0;
+				}
+			}
+			Console.WriteLine(highestCalories);
+		}
+	}
+}
---
+++ Day1/Part2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day1
+{
+	class Part2
+	{
+		public static void Run()
+		{
+			var input = File.ReadAllLines("../../../Day1/Input.txt");
+
+			var totalCalories = 0;
+			var highestThree = 0;
+			List<int> totals = new List<int>();
+
+			foreach (var line in input)
+			{
+				if (line != "")
+				{
+					totalCalories += Int32.Parse(line);
+				}
+				else
+				{
+					totals.Add(totalCalories);
+					totalCalories = 0;
+				}
+			}
+
+			totals.Sort();
+			totals.Reverse();
+
+			highestThree += totals[0];
+			highestThree += totals[1];
+			highestThree += totals[2];
+
+			Console.WriteLine(highestThree);
+		}
+	}
+}
