691cd2fe3a58c8a11c1d0ca464225cdfb85dded2
Daniel Kenny <daniel.kenny@impartner.com>
2022-12-06 20:27:32 +0000

    Day 6 Completed

---
+++ Day6/Part1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day6
+{
+	class Part1
+	{
+		public static void Run()
+		{
+			var input = File.ReadAllLines("../../../Day6/Input.txt");
+
+			foreach(var line in input)
+			{
+				for (int i = 0; i < line.Length; i++)
+				{
+					var markerTrial = line.Substring(i, 4);
+					var groups = markerTrial.GroupBy(c => c).Where(g => g.Count() > 1);
+					if (groups.Count() == 0)
+					{
+						Console.WriteLine(markerTrial);
+						Console.WriteLine(i + 4);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2022
 	{
 		static void Main(string[] args)
 		{
-			Day5.Part2.Run();
+			Day6.Part2.Run();
 		}
 	}
 }
